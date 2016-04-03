a67d733c089ef353ec634cb0959d8826762de63d
ArialBlack <andreyturik@gmail.com>
2016-04-03 23:13:26 +0300

    week2

--- ionic/conFusion/www/js/app.js
+++ ionic/conFusion/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.services'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -25,19 +25,19 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  .state('app', {
+    .state('app', {
     url: '/app',
     abstract: true,
     templateUrl: 'templates/sidebar.html',
     controller: 'AppCtrl'
   })
 
-.state('app.home', {
+  .state('app.home', {
     url: '/home',
     views: {
       'mainContent': {
         templateUrl: 'templates/home.html',
-          controller: 'IndexController'
+        controller: 'IndexController'
       }
     }
   })
@@ -51,16 +51,15 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
         }
       }
     })
-
-   .state('app.contactus', {
+  .state('app.contactus', {
       url: '/contactus',
       views: {
         'mainContent': {
-          templateUrl: 'templates/contactus.html'
+          templateUrl: 'templates/contactus.html',
+          controller: 'ContactController'
         }
       }
     })
-
     .state('app.menu', {
       url: '/menu',
       views: {
@@ -70,7 +69,17 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
         }
       }
     })
-
+  
+  .state('app.favorites', {
+      url: '/favorites',
+      views: {
+        'mainContent': {
+          templateUrl: 'templates/favorites.html',
+          controller:'FavoritesController'
+        }
+      }
+    })
+  
   .state('app.dishdetails', {
     url: '/menu/:id',
     views: {
@@ -79,19 +88,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers','conFusion.service
         controller: 'DishDetailController'
       }
     }
-  })
-  
-  .state('app.favorites', {
-      url: '/favorites',
-      views: {
-        'mainContent': {
-          templateUrl: 'templates/favorites.html',
-            controller:'FavoritesController'
-        }
-      }
-    });
-
+  });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
-
 });
--- ionic/conFusion/www/js/services.js
+++ ionic/conFusion/www/js/services.js
@@ -34,7 +34,7 @@ angular.module('conFusion.services', ['ngResource'])
         .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
     
     
-            return $resource(baseURL+"leadership/:id",null,  {'update':{method:'PUT' }});
+            return $resource(baseURL+"leadership/:id");
     
         }])
 
@@ -44,33 +44,32 @@ angular.module('conFusion.services', ['ngResource'])
             return $resource(baseURL+"feedback/:id");
     
         }])
-        
-        
+
         .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-    var favFac = {};
-    var favorites = [];
+            var favFac = {};
+            var favorites = [];
 
-    favFac.addToFavorites = function (index) {
-        for (var i = 0; i < favorites.length; i++) {
-            if (favorites[i].id == index)
-                return;
-        }
-        favorites.push({id: index});
-    };
-    
-    favFac.deleteFromFavorites = function (index) {
-        for (var i = 0; i < favorites.length; i++) {
-            if (favorites[i].id == index) {
-                favorites.splice(i, 1);
+            favFac.addToFavorites = function (index) {
+                for (var i = 0; i < favorites.length; i++) {
+                    if (favorites[i].id == index)
+                        return;
+                }
+                favorites.push({id: index});
+            };
+            
+            favFac.deleteFromFavorites = function (index) {
+                for (var i = 0; i < favorites.length; i++) {
+                    if (favorites[i].id == index) {
+                        favorites.splice(i, 1);
+                    }
+                }
             }
-        }
-    }
 
-    favFac.getFavorites = function () {
-        return favorites;
-    };
+            favFac.getFavorites = function () {
+                return favorites;
+            };
 
-    return favFac;
-    }])
+            return favFac;
+        }])
 
 ;
