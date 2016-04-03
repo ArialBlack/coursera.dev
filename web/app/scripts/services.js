a7e981092f1cbe760a744f8c6e5b166b5ba1e1a8
ArialBlack <andreyturik@gmail.com>
2016-04-03 20:05:25 +0300

    init

---
+++ web/app/scripts/services.js
@@ -0,0 +1,51 @@
+angular.module('confusionApp')
+    .constant("baseURL", "http://localhost:3000/")
+
+.service('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    
+    this.getPromotions = function () {
+        return $resource(baseURL + "promotions/:id", null, {
+            'update': {
+                method: 'PUT'
+            }
+        });
+    };
+
+    this.getDishes = function () {
+        return $resource(baseURL + "dishes/:id", null, {
+            'update': {
+                method: 'PUT'
+            }
+        });
+    };
+    
+}])
+
+.factory('feedbackFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+    
+    var feedbackfac = {};
+    
+    feedbackfac.getFeedback = function () {
+        return $resource(baseURL + "feedback", null, {
+            'save': {
+                method: 'POST'
+            }
+        });
+    };
+    return feedbackfac;
+}])
+
+.factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+
+    var corpfac = {};
+
+    corpfac.getLeaders = function () {
+        return $resource(baseURL + "leadership/:id", null, {
+            'update': {
+                method: 'PUT'
+            }
+        });
+    };
+    return corpfac;
+
+}]);
