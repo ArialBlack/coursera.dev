99c4df88e7d34908dfeaca966698c4584cbcb874
ArialBlack <andreyturik@gmail.com>
2016-05-08 19:44:21 +0300

    week-2 submission

---
+++ node-mongoose/promoServer.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose'),
+    assert = require('assert');
+
+var Promotiones = require('./models/promotions');
+
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+
+    console.log("Connected correctly to server");
+
+    Promotiones.create({
+        name: 'Weekend Grand Buffet',
+        image: 'images/buffet.png',
+        label: '',
+        price: '19.99',
+        description: 'Featuring......'
+    }, function (err, promotion) {
+        if (err) throw err;
+        console.log('Promotion created!');
+        console.log(promotion);
+
+        var id = promotion._id;
+
+        setTimeout(function () {
+            Promotiones.findByIdAndUpdate(id, {
+                    $set: {
+                        description: 'Updated Promotion'
+                    }
+                }, {
+                    new: true
+                })
+                .exec(function (err, promotion) {
+                    if (err) throw err;
+                    console.log('Updated Promotion!');
+                    console.log(promotion);
+
+                    db.collection('promotions').drop(function () {
+                        db.close();
+                    });
+                });
+        }, 3000);
+    });
+});
--- node-mongoose/server-3.js
+++ node-mongoose/server-3.js
@@ -3,15 +3,14 @@ var mongoose = require('mongoose'),
 
 var Dishes = require('./models/dishes-3');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/conFusion';mongoose.connect(url);
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    // we're connected!
+
     console.log("Connected correctly to server");
 
-    // create a new dish
     Dishes.create({
         name: 'Uthapizza',
         description: 'Test',
@@ -29,7 +28,6 @@ db.once('open', function () {
 
         var id = dish._id;
 
-        // get all the dishes
         setTimeout(function () {
             Dishes.findByIdAndUpdate(id, {
                     $set: {
