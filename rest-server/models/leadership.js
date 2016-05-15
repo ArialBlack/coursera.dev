19398c8f9a7fd2868236d2289b82f1e129f1d1bb
ArialBlack <andreyturik@gmail.com>
2016-05-15 16:34:05 +0300

    week3

---
+++ rest-server/models/leadership.js
@@ -0,0 +1,38 @@
+// grab the things we need
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    designation: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    abbr: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
---
+++ rest-server/models/promotions.js
@@ -0,0 +1,41 @@
+// grab the things we need
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    label: {
+        type: String,
+        default: '',
+        required: false,
+        unique: true
+    },
+    price: {
+        type: Currency,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        unique: true
+    }
+}, {
+    timestamps: true
+});
+
+var Promotions = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotions;
